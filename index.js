bd5bfe1f1de229734a5614c610350b018c6394b8
Alexandra Romano <alex.romano80@gmail.com>
2022-12-16 19:38:57 -0700

    commit tree 62b4f27458b31de9a2ae226045ab7cba4fb9ab0e

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const multiply = 62;
+const num1 =31;
+const num2 = 2;
+//multiply(num1*num2);
+
+const random=Math.floor(Math.random() *1000) + 1;
+
+
+const mod = 4;
+const num3 = 8;
+const num4 = 2;
+//remainder(num3%num4);
+
+const max = 20;
+const set = (18,19,20);
+//max(Math.max(set));
 
 // Write your code here
